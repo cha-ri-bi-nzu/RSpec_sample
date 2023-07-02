814e21bd39368a21b96a985a018bf8c1b03149c9
cha-ri-bi-nzu <kya_hitoyasumisa_n@yahoo.co.jp>
2023-07-02 21:09:23 +0900

    route設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,10 @@
 Rails.application.routes.draw do
-  get 'pitures/index'
-  get 'pitures/new'
-  get 'pitures/create'
-  get 'pitures/confirm'
-  get 'pitures/show'
-  get 'pitures/edit'
-  get 'pitures/update'
-  get 'pitures/destroy'
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
-  get 'users/new'
-  get 'users/create'
-  get 'users/show'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root "pictures#index"
+  resources :pictures do
+    collection do
+      post :confirm
+    end
+  end
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create, :show]
 end
