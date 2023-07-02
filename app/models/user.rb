491e463301f5e00a70b1876aab989425ca5ec148
cha-ri-bi-nzu <kya_hitoyasumisa_n@yahoo.co.jp>
2023-07-02 18:54:44 +0900

    各model定義

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  before_validation { email.downcase! }
+  validates :name, presence: {message: "空白以外の文字を入力して下さい(>_<)"}, length: { maximum: 30 }
+  validates :email, presence: {message: "空白以外の文字を入力して下さい(>_<)"}, length: { maximum: 255}
+  validates :password, length: { minimum: 6 }
+    
+  has_secure_password
+  has_many :pictures
 end
