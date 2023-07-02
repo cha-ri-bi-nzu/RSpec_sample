40b8d3cbcc59e25f17b95c4db278ea73ac71d015
cha-ri-bi-nzu <kya_hitoyasumisa_n@yahoo.co.jp>
2023-07-02 19:03:25 +0900

    controller・view作成

---
+++ app/controllers/pitures_controller.rb
@@ -0,0 +1,25 @@
+class PituresController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def confirm
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  get 'pitures/index'
+  get 'pitures/new'
+  get 'pitures/create'
+  get 'pitures/confirm'
+  get 'pitures/show'
+  get 'pitures/edit'
+  get 'pitures/update'
+  get 'pitures/destroy'
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
+  get 'users/new'
+  get 'users/create'
+  get 'users/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
